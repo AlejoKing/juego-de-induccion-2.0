112f8b20cb988f369816fc9df6cae9c2300d21c2
RicardoTorresits <ricardo.torresits@gmail.com>
2022-05-25 10:46:25 -0500

    coneccoin con la base de datos lista

--- back end/src/database.js
+++ back end/src/database.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 
-mongoose.connect("mongodb://localhost/it-seekers",{
-    usenewUrlParser:true,
+mongoose.connect("mongodb+srv://root:admin123@it-seekers.q8lvn.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
     useUnifiedTopology:true
 })
-    .then(db => console.log('db is connected'))
+    .then(db => console.log('Db is connected'))
     .catch(error=> console.log(error))
