2dec727950e434575dd01bbbf1aab450e9da2d06
RicardoTorresits <ricardo.torresits@gmail.com>
2022-05-31 12:58:46 -0500

    falla la verificacion de el rol

--- back end/src/middlewares/verifySingup.js
+++ back end/src/middlewares/verifySingup.js
@@ -5,7 +5,8 @@ import User from '../models/User';
 export const  checkDuplicateEmail = async(req,res,next) =>{
     try {
         const email = await User.findOne({tEmail:req.body.tEmail})
-    if(email) return  res.status(400).json({message:'the email already exits'})
+    if(email) 
+return  res.status(400).json({message:'the email already exits'})
     next();
     } catch (error) {
         res.status(500).json({message: error})
@@ -17,7 +18,7 @@ export const checkRolesExisted =(req,res,next) =>{
         for (let i=0; i<req.body.roles.length;i++){
             if (!ROLES.includes(req.body.roles[i])){
                 return res.status(400).json({
-                    message:`Role ${req.body.roles[i]}does not exists`
+                    message:`Role ${req.body.roles[i]} does not exists`
                 })
             }
         } 
