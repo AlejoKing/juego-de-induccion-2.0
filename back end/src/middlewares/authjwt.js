8a67371461b604c345fb7ed70a6f802fd75cb133
RicardoTorresits <ricardo.torresits@gmail.com>
2022-05-30 21:53:52 -0500

    midlewares agregados y funcionando

--- back end/src/middlewares/authjwt.js
+++ back end/src/middlewares/authjwt.js
@@ -1,10 +1,45 @@
 import { json } from "body-parser"
+import jwt from "jsonwebtoken"
+import { async } from "regenerator-runtime";
+import config from '../config';
+import User from "../models/User";
+import Role from "../models/Role";
 
 
 export const verifyToken = async (req,res,next) =>{
-    const token =req.headers["x-access-token"]
+    
+    try {
+        const token =req.headers["x-access-token"]
 
-    console.log(token)
+        console.log(token)
 
-    if(!token) return res.status(403),json({message: "no Token provided"})
+        if(!token) return res.status(403),json({message: "no Token provided"})
+
+        const decoded=jwt.verify(token,config.SECRET)
+        req.userId=decoded.id;
+
+        const user = await User.findById(req.userId,{tPassword:0})
+
+        if(!user) return res.status(404).json({message: 'no user found'})
+
+        next()
+    } catch (error) {
+        return res.status(401).json({message:'unauthorized'})
+    }
+
+    
+    
+};
+export const isAdmin = async (req,res,next)=>{
+        const user = await user.findById(req.userId)
+        const roles = await Role.find({_id:{$in: user.roles}})
+
+        for(let i =0; i<roles.length; i++ ){
+            if(roles[i].name ==="admin"){
+                next()
+                return;
+            }
+        }
+
+        return res.status(403).json({message: "need admin rol"})
 }
--- back end/src/routes/auth.routes.js
+++ back end/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
+import auth from 'basic-auth';
 import{Router} from 'express'
 const router =Router()
 
 import * as autCtrl from "../controllers/auth.controller";
+import { authJwt, authjwt } from '../middlewares';
 
-router.post('/signUp',autCtrl.singUp)
+router.post('/signUp',[authJwt.verifyToken, authJwt.isAdmin ],autCtrl.singUp)
 router.post('/signIn', autCtrl.signIn )
 
 export default router;
