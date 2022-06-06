5178b55bc12899d325631d4764df666830626b59
LAPTOP-485BUA3E\Kevin <kevin.murilloits@gmail.com>
2022-06-06 12:15:31 -0500

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={require("./robot.png")} alt="Logo" className="robot" />
+      <div className="container">
+        <img src={require("./its.png")} alt="Logo" />
+        <input type="email" placeholder="e-mail" />
+        <input type="password" placeholder="password" />
+        <button>Send</button>
+      </div>
     </div>
   );
 }
