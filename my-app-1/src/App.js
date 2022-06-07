673973c732b2d995ab5ecd77a4823d4ba02e8c73
AlejoKing <105510539+AlejoKing@users.noreply.github.com>
2022-06-07 18:42:28 -0500

    Administracion de usuarios

---
+++ my-app-1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './componentes/hero'
+
+const App = () => {
+  return (
+    <div>App
+      <Hero></Hero>
+    </div>
+    
+  )
+}
+
+export default App
