221e471fb61538a0976eb14fa3ae978be8bc022f
RicardoTorresits <ricardo.torresits@gmail.com>
2022-06-08 13:51:24 -0500

    cambios al back para reiniicar la password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import Login from "./screens/login/login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
-  return <Login></Login>;
+  /*return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );*/ return <Login></Login>;
+
 }
 
 export default App;
